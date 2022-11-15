68db10c04c7f6f495e431d7779722197e54a97b3
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-11-14 22:11:59 -0800

    sanity publish backend

---
+++ App_backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: "user",
+  title: "User",
+  type: "document",
+  field: [
+    {
+      name: "userName",
+      titile: "UserName",
+      type: "string",
+    },
+    {
+      name: "image",
+      titile: "Image",
+      type: "string",
+    },
+  ],
+};
