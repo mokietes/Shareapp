eb7a809fc504fa4d0aa40a21447eeb5a7a94d2e3
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-11-15 00:35:16 -0800

    update

--- app_frontend/src/index.js
+++ app_frontend/src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
-import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
+import "./index.css";
 import App from "./App";
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
