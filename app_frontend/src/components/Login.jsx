a8aed10fbd9e723e8bc9a036ef298de08f93ec87
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-11-16 01:50:28 -0800

    token upadat

--- app_frontend/src/components/Login.jsx
+++ app_frontend/src/components/Login.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import GoogleLogin from "react-google-login";
-//import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
 
+import { client } from "../client";
+
 const Login = () => {
+  const navigate = useNavigate();
   const responseGoogle = (response) => {
-    console.log(response);
+    localStorage.setItem("user", JSON.stringify(response.profileObj));
+
+    const { name, googleID, imageUrl } = response.profileObj;
+
+    const doc = {
+      _id: googleID,
+      _type: "user",
+      userName: name,
+      image: imageUrl,
+    };
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
   };
   return (
     <div className="flex justify-start items-center flex-col h-screen">
--- app_frontend/src/index.js
+++ app_frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDom from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDom.render(
   <Router>
     <App />
-  </Router>
+  </Router>,
+  document.getElementById("root")
 );
