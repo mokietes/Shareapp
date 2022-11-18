b04118b4996598aacd08f108038accfe42eaf70c
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-11-17 23:56:33 -0800

    Update Home.jsx

--- app_frontend/src/container/Home.jsx
+++ app_frontend/src/container/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from "react";
+import { HiMenu } from "react-icons/hi";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { Link, Route, Routes } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  return <div>Home</div>;
+};
 
-export default Home
+export default Home;
