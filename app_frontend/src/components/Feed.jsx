e5ac279a457ad95350da5b116280a9e754173fae
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-11-26 23:43:10 -0800

    query

--- app_frontend/src/components/Feed.jsx
+++ app_frontend/src/components/Feed.jsx
@@ -2,11 +2,26 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import { client } from "../client";
+import { searchQuery } from "../utils/data";
 import MasonryLayout from "./MasonryLayout";
 import Spinner from "./Spinner";
 
 const Feed = () => {
   const [loading, setLoading] = useState(true);
+  const [pins, setPins] = useState(null);
+
+  const { catagoryId } = useParams();
+  useEffect(() => {
+    setLoading(true);
+    if (catagoryId) {
+      const query = searchQuery(catagoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+    }
+  }, [catagoryId]);
 
   if (loading) return <Spinner message="We are adding new ideas!" />;
   return <div>Feed</div>;
