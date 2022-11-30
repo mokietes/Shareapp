efd97012197a1d356f18205fbed0499f9885a86e
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-11-30 00:08:21 -0800

    Update Pin.jsx

--- app_frontend/src/components/Pin.jsx
+++ app_frontend/src/components/Pin.jsx
@@ -10,7 +10,7 @@ import { fetchUser } from "../utils/fetchUser";
 
 const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
   const [postHover, setPostHover] = useState(false);
-  const [savingPost, setSavingPost] = useState(false);
+  //const [savingPost, setSavingPost] = useState(false);
   const navigate = useNavigate();
   const user = fetchUser();
   const alreadySaved = !!save?.save?.filter(
@@ -18,7 +18,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
   )?.length;
   const savePin = (id) => {
     if (!alreadySaved) {
-      setSavingPost(true);
+      //setSavingPost(true);
       client
         .patch(id)
         .setIfMissing({ save: [] })
@@ -34,9 +34,14 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
         .commit()
         .then(() => {
           window.location.reload();
-          setSavingPost(false);
+          //setSavingPost(false);
         });
     }
+  };
+  const deletePin = (id) => {
+    client.delete(() => {
+      window.location.reload();
+    });
   };
 
   return (
@@ -57,35 +62,64 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
             className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-1 pt-2 pd-2 z-index-50"
             style={{ height: "100%" }}
           >
-            <div className="flex gap-2">
-              <a
-                href={`${image?.asset?.url}?dl=`}
-                download
-                onClick={(e) => e.stopPropagation()}
-                className="bg-white w-9 h-9 p-2 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
-              >
-                <MdDownloadForOffline />
-              </a>
+            <div className="flex items-center justify-between">
+              <div className="flex gap-2">
+                <a
+                  href={`${image?.asset?.url}?dl=`}
+                  download
+                  onClick={(e) => e.stopPropagation()}
+                  className="bg-white w-9 h-9 p-2 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+                >
+                  <MdDownloadForOffline />
+                </a>
+              </div>
+              {alreadySaved ? (
+                <button
+                  type="button"
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3x1 hover:shadow-md outlined-none"
+                >
+                  {save?.length}Saved
+                </button>
+              ) : (
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    savePin(_id);
+                  }}
+                  type="button"
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3x1 hover:shadow-md outlined-none"
+                >
+                  Save
+                </button>
+              )}
             </div>
-            {alreadySaved ? (
-              <button
-                type="button"
-                className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3x1 hover:shadow-md outlined-none"
-              >
-                {save?.length}Saved
-              </button>
-            ) : (
-              <button
-                onClick={(e) => {
-                  e.stopPropagation();
-                  savePin(_id);
-                }}
-                type="button"
-                className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3x1 hover:shadow-md outlined-none"
-              >
-                Save
-              </button>
-            )}
+            <div className="flex justify-between items-center gap-2 w-full">
+              {destination && (
+                <a
+                  href={destination}
+                  target="blank"
+                  rel="noreferrer"
+                  className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:100 hover:shadow-md"
+                >
+                  <BsFillArrowUpRightCircleFill />{" "}
+                  {destination.length > 20
+                    ? destination.slice(8, 22)
+                    : destination.slice(8)}
+                </a>
+              )}
+              {postedBy?.id === user.googleId && (
+                <button
+                  type="button"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deletePin(_id);
+                  }}
+                  className="bg-white-500 p-2 opacity-70 hover:opacity-100 text-dark font-bold px-5 py-1 text-base rounded-3x1 hover:shadow-md outlined-none"
+                >
+                  <AiTwotoneDelete />
+                </button>
+              )}
+            </div>
           </div>
         )}
       </div>
