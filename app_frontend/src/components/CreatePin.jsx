33640278e16283d24ecec28ac671e3ad2af8a390
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-11-30 23:57:17 -0800

    Update CreatePin.jsx

--- app_frontend/src/components/CreatePin.jsx
+++ app_frontend/src/components/CreatePin.jsx
@@ -12,14 +12,22 @@ const CreatePin = ({ user }) => {
   const [about, setAbout] = useState("");
   const [destination, setDestination] = useState("");
   const [loading, setLoading] = useState(false);
-  const [fields, setFields] = useState(null);
+  const [fields, setFields] = useState(true);
   const [catagory, setCatagory] = useState(null);
   const [imageAsset, setImageAsset] = useState(null);
   const [wrongImageType, setWrongImageType] = useState(false);
 
   const navigate = useNavigate();
 
-  return <div>CreatePin</div>;
+  return (
+    <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
+      {fields && (
+        <p className="text-red-500 mb-5 text-xl transition-all duration-150 ease-in">
+          Please fill in all the field
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default CreatePin;
