cf85aee2fe5b15f519e1fc0f64ca386c6e8e7ac7
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-12-01 01:05:34 -0800

    Update CreatePin.jsx

--- app_frontend/src/components/CreatePin.jsx
+++ app_frontend/src/components/CreatePin.jsx
@@ -15,9 +15,37 @@ const CreatePin = ({ user }) => {
   const [fields, setFields] = useState(false);
   const [catagory, setCatagory] = useState(null);
   const [imageAsset, setImageAsset] = useState(null);
-  const [wrongImageType, setWrongImageType] = useState(true);
+  const [wrongImageType, setWrongImageType] = useState(false);
 
   const navigate = useNavigate();
+
+  const uploadImage = (e) => {
+    const { type, name } = e.target.files[0];
+    if (
+      type === "image/png" ||
+      type === "image/svg" ||
+      type === "image/jpeg" ||
+      type === "image/gif" ||
+      type === "image/tiff"
+    ) {
+      setWrongImageType(false);
+      setLoading(true);
+      client.assets
+        .upload("image", e.target.files[0], {
+          contentType: type,
+          filename: name,
+        })
+        .then((document) => {
+          setImageAsset(document);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log("Image upload error", error);
+        });
+    } else {
+      setWrongImageType(true);
+    }
+  };
 
   return (
     <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
@@ -31,6 +59,42 @@ const CreatePin = ({ user }) => {
           <div className=" flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
             {loading && <Spinner />}
             {wrongImageType && <p>Wrong Image Type</p>}
+            {!imageAsset ? (
+              <label>
+                <div className="flex flex-col items-center justify-center h-full">
+                  <div className="flex flex-col justify-center items-center">
+                    <p className="font-bold text-2xl">
+                      <AiOutlineCloudUpload />
+                    </p>
+                    <p className="text-lg">Click to upload</p>
+                  </div>
+                  <p className="mt-32 text-gray-400">
+                    use high-quality JPG, SVG, PNG, GIF or TIFF less than 20 MB
+                  </p>
+                </div>
+                <input
+                  type="file"
+                  name="upload-image"
+                  onChange={uploadImage}
+                  className="w-0 h-0"
+                />
+              </label>
+            ) : (
+              <div className="relative h-full">
+                <img
+                  src={imageAsset?.url}
+                  alt="uploaded-pic"
+                  className="h-full w-full"
+                />
+                <button
+                  type="button"
+                  className="absolute bottom-3 bottom-3 right-3 rounded-full p-3 bg-white text-xl crusor-pointer outline-none hover:shodow-md transition-all duration-500 ease-in-out "
+                  onClick={() => setImageAsset(null)}
+                >
+                  <MdDelete />
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
