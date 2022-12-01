e8dd090b04f1bfd7909e2e2f12d2fac3f39da98f
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-12-01 01:14:22 -0800

    Update CreatePin.jsx

--- app_frontend/src/components/CreatePin.jsx
+++ app_frontend/src/components/CreatePin.jsx
@@ -97,6 +97,32 @@ const CreatePin = ({ user }) => {
             )}
           </div>
         </div>
+        <div className="flex flex-1 flex-col gap-6 lg:pl-5 mt-5 w-full">
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Add your title here"
+            className="outline-none text-2xl sm:text-3xl font-bold boarder-b-2 border-gray-200 p-2"
+          />
+          {user && (
+            <div className="flex gap-2 my-2 my-2 items-center bg-white rounded-lg">
+              <img
+                src={user.image}
+                classname="w-10 h-10 rounded-full"
+                alt="user-profile"
+              />
+              <p className="font-bold">{user.userName}</p>
+              <input
+                type="text"
+                value={about}
+                onChange={(e) => setAbout(e.target.value)}
+                placeholder="What is your pin about"
+                className="outline-none text-base sm:text-lg font-bold boarder-b-2 border-gray-200 p-2"
+              />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
