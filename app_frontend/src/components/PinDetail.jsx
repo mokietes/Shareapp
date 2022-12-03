f9a9d9b32163653822fd360484f84091ae5f24c2
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-12-03 00:23:11 -0800

    Update PinDetail.jsx

--- app_frontend/src/components/PinDetail.jsx
+++ app_frontend/src/components/PinDetail.jsx
@@ -16,6 +16,31 @@ const PinDetail = ({ user }) => {
   const [addingComment, setAddingComment] = useState(false);
   const { pinId } = useParams();
 
+  const addComment = () => {
+    if (comment) {
+      setAddingComment(true);
+      client
+        .patch(pinId)
+        .setIfMissing({ comment: [] })
+        .insert("after", "comment[-1]", [
+          {
+            comment,
+            _key: uuidv4(),
+            postedBy: {
+              _type: "postedBy",
+              _ref: user._id,
+            },
+          },
+        ])
+        .commit()
+        .then(() => {
+          fetchPinDetails();
+          setComment("");
+          setAddingComment(false);
+        });
+    }
+  };
+
   const fetchPinDetails = () => {
     const query = pinDetailQuery(pinId);
 
@@ -97,8 +122,35 @@ const PinDetail = ({ user }) => {
                 alt="user-profile"
                 className="w-10 h-10 rounded-full cursor-pointer"
               />
+              <div className="flex flex-col">
+                <p className="font-bold">{comment.postedBy.userName}</p>
+                <p>{comment.comment}</p>
+              </div>
             </div>
           ))}
+        </div>
+        <div className="flex flex-wrap mt-6 gap-3">
+          <Link to={`user-profile/${pinDetail.postedBy?._id}`}>
+            <img
+              className="w-10 h-10 rounded-full coursor-pointer"
+              src={pinDetail.postedBy?.image}
+              alt="user-profile"
+            />
+          </Link>
+          <input
+            className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus:border-gray-300"
+            type="text"
+            placeholder="Add a comment"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <button
+            type="button"
+            className="bg-red-500 text-white rounded-full px-6 font-semibold text-base outline-none "
+            onClick={addComment}
+          >
+            {addingComment ? "Posting the comment..." : "Post"}
+          </button>
         </div>
       </div>
     </div>
