3508cb264e9e789d573d4cd852c3671d84f3e0c0
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-12-02 23:27:49 -0800

    Update PinDetail.jsx

--- app_frontend/src/components/PinDetail.jsx
+++ app_frontend/src/components/PinDetail.jsx
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { IoMdRocket } from "react-icons/io";
+import { MdDownloadForOffline } from "react-icons/md";
+import { Link, useParamas } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
-const PinDetail = () => {
+import { client, urlFor } from "../client";
+import MasonryLayout from "./MasonryLayout";
+import { pinDetailMorePinQuery, pinDetailQuery } from "../utils/data";
+import Spinner from "./Spinner";
+
+const PinDetail = ({ user }) => {
+  const [Pins, setPins] = useState(null);
+  const [pinDetail, setPineDetail] = useState(null);
+  const [comment, setComment] = useState("");
+  const [addingComment, setAddingComment] = useState(false);
+  const { pinId } = useParamas();
+
+  if (!pinDetail) return <spinner message="Loading pin" />;
+
+  const fetchPinDetails = () => {
+    const query = pinDetailQuery(pinId);
+
+    if (query) {
+      client.fetch(query).then((data) => {
+        setPineDetail(data[0]);
+
+        if (data[0]) {
+          query = pinDetailMorePinQuery(data[0]);
+
+          client.fetch(query).then((res) => setPins(res));
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchPinDetails();
+  }, [pinId]);
+
   return <div>PinDetail</div>;
 };
 
