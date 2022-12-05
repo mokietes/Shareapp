41ea87bb01bed5ecfeec52d80d84e0580b83550c
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-12-05 00:22:22 -0800

    debug

--- app_frontend/src/components/UserProfile.jsx
+++ app_frontend/src/components/UserProfile.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { AiOutlineLogout } from "react-icons/ai";
 import { useParams, useNavigate } from "react-router-dom";
 import { googleLogout } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import {
   userCreatedPinsQuery,
@@ -13,7 +14,7 @@ import MasonryLayout from "./MasonryLayout";
 import Spinner from "./Spinner";
 
 const randomImage =
-  "https://source.unsplash.com/1600*900/?nature,photography,technology";
+  "https://source.unsplash.com/1600x900/?nature,photography,technology";
 
 const UserProfile = () => {
   const [user, setUser] = useState();
@@ -31,24 +32,50 @@ const UserProfile = () => {
     });
   }, [userId]);
 
+  const logout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
+
   if (!user) {
     return <Spinner message="Loading Profile.." />;
   }
 
   return (
-    <div className="relative pd-2 h-full justify-center items-center ">
-      <div className="flex flex-col pb-5">
-        <div className="relative flex flex-col mb-7">
-          <div className="flex flex-col justify-center items-center">
-            <img
-              src={randomImage}
-              className="w-full h-370 2xl:h-510 shadow-lg object-cover"
-              alt="bannerPic"
-            />
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+      <div className="relative pd-2 h-full justify-center items-center ">
+        <div className="flex flex-col pb-5">
+          <div className="relative flex flex-col mb-7">
+            <div className="flex flex-col justify-center items-center">
+              <img
+                src={randomImage}
+                className="w-full h-370 2xl:h-510 shadow-lg object-cover"
+                alt="bannerPic"
+              />
+              <img
+                className="rounded-full w-20 h-20 -mt-10 shadow-xl object-cover"
+                src={user.image}
+                alt="user-pic"
+              />
+              <h1 className="font-bold text-3xl text-center mt-3">
+                {user.userName}
+              </h1>
+              <div className="absolute top-0 z-1 right-0 p-2">
+                {userId === user._id && (
+                  <button
+                    type="button"
+                    className="bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
+                    onClick={logout}
+                  >
+                    <AiOutlineLogout color="red" fontSize={21} />
+                  </button>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 };
 
