a9729784d5e87346db211260d11415e15972f7de
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-12-06 23:24:40 -0800

    Update Search.jsx

--- app_frontend/src/components/Search.jsx
+++ app_frontend/src/components/Search.jsx
@@ -8,6 +8,23 @@ import Spinner from "./Spinner";
 const Search = ({ searchTerm }) => {
   const [pins, setPins] = useState();
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchTerm !== "") {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
   return (
     <div>
       {loading && <Spinner message="Searching pins" />}
