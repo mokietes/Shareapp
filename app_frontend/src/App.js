042ddabba5cd84b6d2e12d525e525e455f5455c9
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2023-02-11 17:51:40 -0800

    final upload

--- app_frontend/src/App.js
+++ app_frontend/src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+//import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import Login from "./components/Login";
 import Home from "./container/Home";
 
+import { fetchUser } from "./utils/fetchUser";
+
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+
+    if (!user) navigate("./login");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Routes>
       <Route path="login" element={<Login />} />
